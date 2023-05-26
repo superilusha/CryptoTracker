594f25720f94788ef4455508a1951cb559faf38b
superilusha <anoshin.ilya@gmail.com>
2023-05-27 00:15:21 +0300

    temporary fix of filters line

--- CryptoTracker/Views/CoinsList/CoinsListViewController.swift
+++ CryptoTracker/Views/CoinsList/CoinsListViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class CoinsListViewController: UIViewController {
     
+    
+    
+    private enum SortMode {
+        case noSort
+        case ascending
+        case descending
+    }
+
     private let tableView: UITableView = {
         let table = UITableView()
         table.register(UITableViewCell.self, forCellReuseIdentifier: "coinCell")
@@ -21,9 +29,30 @@ class CoinsListViewController: UIViewController {
         indicator.hidesWhenStopped = true
         return indicator
     }()
-
     
+    private let tickerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Ticker", for: .normal)
+        return button
+    }()
+    
+    private let priceButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Price", for: .normal)
+        return button
+    }()
+    
+    private let changeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Change 24h", for: .normal)
+        return button
+    }()
+
     private var coins: [Coin] = []
+    
+    private var currentTickerSort = SortMode.noSort
+    private var currentPriceSort = SortMode.noSort
+    private var currentChangeSort = SortMode.noSort
     
     private var presenter: CoinsListPresenter!
     
@@ -36,6 +65,14 @@ class CoinsListViewController: UIViewController {
         
         navigationItem.title = "Coins"
         view.addSubview(tableView)
+        view.addSubview(tickerButton)
+        view.addSubview(priceButton)
+        view.addSubview(changeButton)
+        
+        tickerButton.addTarget(self, action: #selector(tickerButtonTapped), for: .touchUpInside)
+        priceButton.addTarget(self, action: #selector(priceButtonTapped), for: .touchUpInside)
+        changeButton.addTarget(self, action: #selector(changeButtonTapped), for: .touchUpInside)
+        
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
@@ -49,64 +86,135 @@ class CoinsListViewController: UIViewController {
             loadingIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
 
-        
-        
+        setupSortButtonsLayout()
     }
+    
+     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+            return 40
+        }
+
+         func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+            let label = UILabel()
+            label.text = ""
+            label.font = UIFont.systemFont(ofSize: 20)
+            label.textColor = .black
+            label.backgroundColor = .white
+
+            return label
+        }
+
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-            tableView.reloadData()
+        tableView.reloadData()
+        fetchCoins()
+    }
+
+    private func setupSortButtonsLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        [tickerButton, priceButton, changeButton].forEach { button in
+            button.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            tickerButton.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            tickerButton.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 24),
             
-            fetchCoins()
-
+            changeButton.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            changeButton.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 8),
+            
+            priceButton.trailingAnchor.constraint(equalTo: changeButton.leadingAnchor, constant: -16),
+            priceButton.centerYAnchor.constraint(equalTo: changeButton.centerYAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: tickerButton.bottomAnchor, constant: 16),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 
+    
     @objc private func refreshTableView() {
-        fetchCoins() {
-            self.refreshControl.endRefreshing()
-        }
-    }
+          fetchCoins() {
+              self.refreshControl.endRefreshing()
+          }
+      }
 
+      private let refreshControl: UIRefreshControl = {
+          let refreshControl = UIRefreshControl()
+          refreshControl.addTarget(self, action: #selector(refreshTableView), for: .valueChanged)
+          return refreshControl
+      }()
+
+      private func setupLogoutButton() {
+          let logoutBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(logoutButtonPressed))
+          navigationItem.rightBarButtonItem = logoutBarButtonItem
+      }
+      
+      @objc private func logoutButtonPressed() {
+          presenter.logout()
+      }
+      
+      private func fetchCoins(completion: (() -> Void)? = nil) {
+          let networkManager = NetworkManager()
+          loadingIndicator.startAnimating()
+
+          networkManager.fetchCoins() { [weak self] result in
+              DispatchQueue.main.async {
+                  self?.loadingIndicator.stopAnimating()
+
+                  switch result {
+                  case .success(let coins):
+                      self?.coins = coins
+                      self?.tableView.reloadData()
+                  case .failure(let error):
+                      print("Error fetching coins: \(error.localizedDescription)")
+                  }
+                  if let completion = completion {
+                      completion()
+                  }
+              }
+          }
+      }
+      
+      @objc func tickerButtonTapped() {
+          switch currentTickerSort {
+          case .noSort, .ascending:
+              coins.sort { $0.symbol < $1.symbol }
+              currentTickerSort = .descending
+          case .descending:
+              coins.sort { $0.symbol > $1.symbol }
+              currentTickerSort = .ascending
+          }
+          tableView.reloadData()
+      }
+
+      @objc func priceButtonTapped() {
+          switch currentPriceSort {
+          case .noSort, .ascending:
+              coins.sort { $0.priceUsd < $1.priceUsd }
+              currentPriceSort = .descending
+          case .descending:
+              coins.sort { $0.priceUsd > $1.priceUsd }
+              currentPriceSort = .ascending
+          }
+          tableView.reloadData()
+      }
+
+      @objc func changeButtonTapped() {
+          switch currentChangeSort {
+          case .noSort, .ascending:
+              coins.sort { $0.percentChangeUsdLast24Hours < $1.percentChangeUsdLast24Hours }
+              currentChangeSort = .descending
+          case .descending:
+              coins.sort { $0.percentChangeUsdLast24Hours > $1.percentChangeUsdLast24Hours }
+              currentChangeSort = .ascending
+          }
+          tableView.reloadData()
+      }
     
-    private let refreshControl: UIRefreshControl = {
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(refreshTableView), for: .valueChanged)
-        return refreshControl
-    }()
-
     
-    private func setupLogoutButton() {
-        let logoutBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(logoutButtonPressed))
-        navigationItem.rightBarButtonItem = logoutBarButtonItem
-    }
-    
-    @objc private func logoutButtonPressed() {
-        presenter.logout()
-    }
-    
-    private func fetchCoins(completion: (() -> Void)? = nil) {
-        let networkManager = NetworkManager()
-        loadingIndicator.startAnimating()
-
-        networkManager.fetchCoins() { [weak self] result in
-            DispatchQueue.main.async {
-                self?.loadingIndicator.stopAnimating()
-                            
-
-                switch result {
-                case .success(let coins):
-                    self?.coins = coins
-                    self?.tableView.reloadData()
-                case .failure(let error):
-                    print("Error fetching coins: \(error.localizedDescription)")
-                }
-                if let completion = completion {
-                    completion()
-                }
-            }
-        }
-    }
-
-}
+  }
 
 extension CoinsListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
